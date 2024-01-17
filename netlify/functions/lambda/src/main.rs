94b090b20841e7f28135922be82fe05b5c8cce9a
617914118@qq.com <617914118@qq.com>
2024-01-17 15:09:02 +0800

    1

--- netlify/functions/lambda/src/main.rs
+++ netlify/functions/lambda/src/main.rs
@@ -1,36 +1,17 @@
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use lamedh_http::{
+    lambda::{lambda, Context},
+    IntoResponse, Request,
+};
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+type Error =
+    Box<dyn std::error::Error + Send + Sync + 'static>;
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
-
+#[lambda(http)]
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
-        .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
-        .without_time()
-        .init();
-
-    run(service_fn(function_handler)).await
+async fn main(
+    _: Request,
+    _: Context,
+) -> Result<impl IntoResponse, Error> {
+    dbg!("in main");
+    Ok("boop")
 }
